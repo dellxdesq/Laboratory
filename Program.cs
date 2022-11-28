63933194b9f1fff7020ea1c81c7a6b4a92687aae
Nikita <117754477+dellxdesq@users.noreply.github.com>
2022-11-28 11:59:21 +0300

    new

--- Program.cs
+++ Program.cs
@@ -3,39 +3,38 @@ using System.Globalization;
 
 class Program
 {
-  
-
     public static void Main()
     {
-        Console.WriteLine("Введите массив ебаный свет!");
+        Console.WriteLine("Введите массив!");
         while (true)
         {
             string inputArrStr = Console.ReadLine() ?? string.Empty;
             var inputArr = new List<string>(inputArrStr.Split(' '));
-            var outputArr = OperateInputArray(inputArr);
+            var outputArr = CocoJambo(inputArr);
             outputArr.ForEach(Console.WriteLine);
         }
     }
 
-
-    public static List<string> OperateInputArray(List<string> inputArr)
+    public static List<long> CocoJambo(List<string> inputArr)
     {
-        var outputArr = new List<string>();
+        var outputArr = new List<long>();
         foreach (var num in inputArr)
         {
             if (IsInt(num) && !IsNegative(num))
             {
                 long res = Fact(long.Parse(num));
-                outputArr.Add(res.ToString());
-            }
-            else if (IsFloat(num))
-            {
-                string res = RoundAndCut(num, 2);
                 outputArr.Add(res);
             }
+
+            else if (IsDouble(num) && num.IndexOf(',') > 0)
+            {
+                long res = Round(double.Parse(num));
+                outputArr.Add(res);
+            }
+
             else
             {
-                outputArr.Add(num);
+                outputArr.Add(long.Parse(num));
             }
         }
         return outputArr;
@@ -45,9 +44,9 @@ class Program
         return int.TryParse(input, out var number);
     }
 
-    public static bool IsFloat(string input)
+    public static bool IsDouble(string input)
     {
-        return float.TryParse(input, out var number);
+        return double.TryParse(input, out var number);
     }
 
     public static bool IsNegative(string number)
@@ -55,50 +54,31 @@ class Program
         bool answer = number[0] == '-';
         return answer;
     }
-    public static string RoundAndCut(string number, int midPointCut)
-    {
-        number = Cut(Round(number, midPointCut));
-        return number;
-    }
-
-    public static string Cut(string number)
-    {
-        string cutNumber;
-
-        number = DeleteZero(number);
-        int pointIndex = number.IndexOf(',');
-        cutNumber = number.Substring(pointIndex + 1, number.Length - pointIndex - 1).TrimStart('0');
-        if (string.IsNullOrEmpty(cutNumber))
-            cutNumber = "0";
-        return cutNumber;
-    }
-
-    public static string DeleteZero(string wrongString)
-    {
-        return string.Join("", wrongString.Split("\0"));
-    }
-
-    public static string Round(string number, int midPointRounding)
-    {
-        string roundedNumber;
-        int pointIndex = number.IndexOf(',');
-        char[] safeNumberArr = new char[number.Length + midPointRounding];
-        for (int i = 0; i < number.Length; ++i)
-            safeNumberArr[i] = number[i];
-
-        if (safeNumberArr[pointIndex + midPointRounding + 1] >= '5')
-        {
-            safeNumberArr[pointIndex + midPointRounding] += (char)('1' - '0');
-        }
-        roundedNumber = new string(safeNumberArr).Substring(0, pointIndex + midPointRounding + 1);
-        return roundedNumber;
-    }
-
+    
     public static long Fact(long n)
     {
         if (n == 0)
             return 1;
         else
             return n * Fact(n - 1);
+    }
+
+    public static int FindNumberOfDigits(double number)
+    {
+        string strNumber = number.ToString();
+        int index = strNumber.IndexOf(",");
+
+        int length = 0;
+        if (index > -1)
+        {
+            length = strNumber.Substring(index).Length - 1;
+        }
+        return length;
+    }
+    public static int Round(double number)
+    {
+        number = Math.Abs(number);
+        double val = Math.Round(number - (int)number, 2) * 100;
+        return (int)val;
     }
 }
