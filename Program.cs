ec7915284dd0419a832885ee1341c318762f9c8d
Igor Babin <igorbabin2002@gmail.com>
2022-12-07 18:43:48 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+// Твоя структура репозитория не соответствует требованиям, в гитлабе указано что вы делаете коммиты в ветки заданий,
+// затем вливаете их в ветку develop (которой у тебя нет) а ее уже в main. По-хорошему, я вообще не должен проверять код в этой ветке
+using System;
 using System.IO.Compression;
 
 class Program
@@ -13,18 +15,22 @@ class Program
         float comparison1 = float.NaN;
         float comparison2 = float.NaN;
         string input = "";
+        // присваивать значение input необязательно, но если ты присваиваешь пустую строку то используй поле string.Empty
         int number = 0;
         while (input != "q")
         {
             input = Console.ReadLine();
 
             if (int.TryParse(input, out number))
+                // number лучше сделать локальной переменной, чтобы она не отнимала память когда не нужна и было проще читать код
             {
                 Console.WriteLine((char)number);
             }
             else if (float.TryParse(input, out comparison2))
+                // то же, что и number
             {
                 if (ComparisonFloats(comparison1, comparison2, (float)0.1))
+                    // ненужное приведение к float, эту переменную можно объявить как 0.1f (первый блок юлерна)
                     break;
                 comparison1 = comparison2;
             }
@@ -32,6 +38,7 @@ class Program
     }
 
     public static bool ComparisonFloats(float f1, float f2, float eps)
+        // некорректное название, методы называются глаголами и отражают свой функционал. Корректные варианты: CompareFloats, FloatEquals
     {
         return Math.Abs(f1 - f2) <= eps;
     }
